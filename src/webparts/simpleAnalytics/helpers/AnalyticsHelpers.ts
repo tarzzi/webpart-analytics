c9dc7c66e1e4099dab14d3d9da5c40e8406bd3f3
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2024-05-03 00:48:39 +0300

    Add getCellValue and getTextCellValue methods to AnalyticsHelper class

--- src/webparts/simpleAnalytics/helpers/AnalyticsHelpers.ts
+++ src/webparts/simpleAnalytics/helpers/AnalyticsHelpers.ts
@@ -1,10 +1,22 @@
 export class AnalyticsHelper {
 
+    /**
+     * Get cell value from selected row
+     * @param row 
+     * @param key 
+     * @returns {number} Selected cell value as number
+     */
     public static getCellValue(row: any, key: string): number {
         const cell = row.Cells.filter((c: any) => c.Key === key);
         return cell.length > 0 ? cell[0].Value : 0;
     }
 
+    /**
+     * Get cell value from selected row
+     * @param row 
+     * @param key 
+     * @returns {string} Selected cell value as string
+     */
     public static getTextCellValue(row: any, key: string): string {
         const cell = row.Cells.filter((c: any) => c.Key === key);
         return cell.length > 0 ? cell[0].Value : '';
