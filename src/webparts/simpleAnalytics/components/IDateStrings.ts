ec4974d3e8cee1f660de188edcb0c7a19fdca3f9
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2024-05-06 23:45:21 +0300

    feat: Add IDateStrings interface for date string definitions

---
+++ src/webparts/simpleAnalytics/components/IDateStrings.ts
@@ -0,0 +1,12 @@
+export interface IDateStrings {
+    last1Day: string;
+    last2Day: string;
+    last3Day: string;
+    last4Day: string;
+    last5Day: string;
+    last6Day: string;
+    last7Day: string;
+    lastMonth1: string;
+    lastMonth2: string;
+    lastMonth3: string;
+  }
